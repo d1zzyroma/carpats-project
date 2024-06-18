65ed4f2d969ae3238864e13676e52b240cd75b69
d1zzyroma <nataliawologin@gmail.com>
2024-06-18 15:53:03 +0200

    321

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-// const heroBtn = document.querySelector(".hero-btn");
-// const backDrop = document.querySelector(".back-drop");
-// const closeBtn = document.querySelector(".modal-menu-close-btn");
+const heroBtn = document.querySelector(".hero-btn");
+const backDrop = document.querySelector(".back-drop");
+const closeBtn = document.querySelector(".modal-menu-close-btn");
 
-// heroBtn.addEventListener("click", handleClick)
+heroBtn.addEventListener("click", handleClick)
 
 
 
-// closeBtn.addEventListener("click", handleClick)
+closeBtn.addEventListener("click", handleClick)
 
-// function handleClick(){
-//   backDrop.classList.toggle("is-open")
-// }
+function handleClick(){
+  backDrop.classList.toggle("is-open")
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   const splide = new Splide('.splide', {
